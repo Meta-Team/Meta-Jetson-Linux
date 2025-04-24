aca9f427ecade52145e577df9e96dd4b98d406b6
Tony Zhang <mcerztn@gmail.com>
2025-04-24 23:27:02 +0800

    init with jetson linux r36.4.3

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT_RT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT_RT Thu Apr 24 21:24:23 CST 2025"
+#define LINUX_COMPILE_BY "ztn"
+#define LINUX_COMPILE_HOST "ztn-MS-7D42"
+#define LINUX_COMPILER "aarch64-buildroot-linux-gnu-gcc.br_real (Buildroot 2022.08) 11.3.0, GNU ld (GNU Binutils) 2.38"
